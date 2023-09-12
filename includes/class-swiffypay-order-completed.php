b010604ebf30fd5bba4839a99a30e2d44c6f7a0d
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-09-12 09:15:11 +0800

    Create class-swiffypay-order-completed.php

---
+++ includes/class-swiffypay-order-completed.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * @wordpress-plugin
+ * Plugin Name:       Swiffypay Order Completed
+ * Plugin URI:        https://yourpropfirm.com/
+ * Description:       Add-On for Swiffypay to change status order to completed 
+ * Version:           1.0.1
+ * Author:            Ardika JM Consulting
+ * Author URI:        https://yourpropfirm.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+*/
